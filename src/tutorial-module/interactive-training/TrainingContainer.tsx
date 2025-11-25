935dfd296640c56a0e362470c2b8b34a90bebc24
gqcorneby <185756521+gqcorneby@users.noreply.github.com>
2025-11-25 12:24:13 +0800

    use switch case for future proofing

--- src/tutorial-module/interactive-training/TrainingContainer.tsx
+++ src/tutorial-module/interactive-training/TrainingContainer.tsx
@@ -15,25 +15,26 @@ type TrainingContainerProps = {
 export const TrainingContainer: React.FC<TrainingContainerProps> = props => {
     const { containerConfig, content, isMinimized, onMinimize, children } = props;
 
-    if (containerConfig.type === "sidebar") {
-        return (
-            <PaneledContainer
-                className={isMinimized ? "" : "show-panel"}
-                isRight={containerConfig.position === "right"}
-                width={containerConfig.width}
-                unit={containerConfig.unit}
-            >
-                <div>{children}</div>
-                <InteractiveTrainingPanel minimized={isMinimized} content={content} onMinimize={onMinimize} />
-            </PaneledContainer>
-        );
-    } else {
-        return (
-            <>
-                {children}
-                <InteractiveTrainingModal minimized={isMinimized} content={content} onMinimize={onMinimize} />
-            </>
-        );
+    switch (containerConfig.type) {
+        case "sidebar":
+            return (
+                <PaneledContainer
+                    className={isMinimized ? "" : "show-panel"}
+                    isRight={containerConfig.position === "right"}
+                    width={containerConfig.width}
+                    unit={containerConfig.unit}
+                >
+                    <div>{children}</div>
+                    <InteractiveTrainingPanel minimized={isMinimized} content={content} onMinimize={onMinimize} />
+                </PaneledContainer>
+            );
+        case "dialog":
+            return (
+                <>
+                    {children}
+                    <InteractiveTrainingModal minimized={isMinimized} content={content} onMinimize={onMinimize} />
+                </>
+            );
     }
 };
 
